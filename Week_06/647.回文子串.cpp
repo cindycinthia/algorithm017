4d0161c724b0c1b8e5d04305375bed13e8e0392e
cindycinthia <chenyu1_1@163.com>
2020-12-13 22:30:26 +0800

    week

---
+++ Week_06/647.回文子串.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=647 lang=cpp
+ *
+ * [647] 回文子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size(), ans = 0;
+        for (int i = 0; i < 2 * n - 1; ++i) {
+            int l = i / 2, r = i / 2 + i % 2;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                --l;
+                ++r;
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+
+// @lc code=end
+
